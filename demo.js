3f744a03804f21b6c67be70cb61aff7adc080057
Aarif1430 <malikarif13@gmail.com>
2020-09-23 10:39:49 +0530

    background width1

--- demo.js
+++ demo.js
@@ -15,7 +15,7 @@
 
   function initHeader() {
     width = window.innerWidth;
-    height = window.innerHeight/2;
+    height = window.innerHeight;
     target = { x: width / 2, y: height / 2 };
 
     largeHeader = document.getElementById("large-header");
